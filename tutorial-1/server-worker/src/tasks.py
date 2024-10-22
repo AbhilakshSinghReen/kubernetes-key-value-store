70a82b76cfa3d94e75f41f96b02b82d992472049
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-10-22 17:20:13 +0530

    Added redis_client and tasks in server-worker.

---
+++ tutorial-1/server-worker/src/tasks.py
@@ -0,0 +1,20 @@
+from os import environ
+
+from huey import RedisHuey
+
+from .redis_client import redis_client
+
+
+redis_host = "localhost" # environ["REDIS_HOST"]
+
+huey = RedisHuey('entrypoint', host=redis_host)
+
+
+@huey.task()
+def set_key_value_pair(key: str, value: str):
+    redis_client.set(key, value)
+
+
+@huey.task()
+def delete_key_value_pair(key: str):
+    redis_client.delete(key)
